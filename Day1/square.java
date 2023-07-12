d6bd72f63713b77cc1aa01a7e21ab1df4ecb879b
SudarsonK2000 <88591463+SudarsonK2000@users.noreply.github.com>
2023-07-12 13:27:51 +0530

    Add files via upload

---
+++ Day1/square.java
@@ -0,0 +1,20 @@
+
+public class square
+{
+    public static void main(String[] args)
+    {
+        int n =5;
+        for (int i=1;i<=5;i++)
+        {
+        for (int k=1;k<=n-i;k++)
+        {
+           System.out.print(".");
+        }
+        for (int j=1;j<=i;j++)
+        {
+            System.out.print(i);
+        }
+         System.out.println();
+        }
+    }
+}
